c7b6553b09762a6f7aff5a210f3b3cdc2952d0b2
Kennybabs-py <babalolakehinde74@gmail.com>
2022-10-16 13:42:37 +0100

    updates; switch statement used

--- src/components/ProgressBar/index.js
+++ src/components/ProgressBar/index.js
@@ -1,16 +1,15 @@
 import "./styles.css";
 import "rsuite/dist/rsuite.min.css";
 import { Steps } from "rsuite";
-import WechatIcon from "@rsuite/icons/Wechat";
 
 const stepsStyles = {
   display: "inline-table",
   verticalAlign: "top",
+  color: "green",
 };
 
 const itemStyles = {
-  height: "100px",
-  border: "1px solid black",
+  height: "80px",
 };
 
 export default function ProgressBar(props) {
@@ -19,25 +18,13 @@ export default function ProgressBar(props) {
   return (
     <div className="progressbar">
       <Steps current={step} vertical style={stepsStyles}>
-        <Steps.Item style={itemStyles} title="Finished" icon={<WechatIcon />} />
+        <Steps.Item style={itemStyles} title="Create an Account" />
 
-        <Steps.Item
-          style={itemStyles}
-          title="In progress"
-          icon={step > 1 && <WechatIcon />}
-        />
+        <Steps.Item style={itemStyles} title="In progress" />
 
-        <Steps.Item
-          style={itemStyles}
-          title="Waiting"
-          icon={step > 2 && <WechatIcon />}
-        />
+        <Steps.Item style={itemStyles} title="Waiting" />
 
-        <Steps.Item
-          style={itemStyles}
-          title="Waiting"
-          icon={step > 3 && <WechatIcon />}
-        />
+        <Steps.Item style={itemStyles} title="Waiting" />
       </Steps>
     </div>
   );
--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import "./styles.css";
 import ProgressBar from "../ProgressBar";
 
@@ -6,10 +5,6 @@ export default function SideBar({ step }) {
   return (
     <div className="sidebar">
       <h1>SIDEBAR</h1>
-      <Link to="/">Sign up</Link>
-      <br />
-
-      <Link to="/auth">Auth</Link>
 
       <ProgressBar step={step} />
     </div>
